cf76e5591039e3fd1651894cd98b4c399d477a00
Titan <santitanxman@yahoo.com>
2016-03-26 14:24:30 +0800

    modify_orientation.tcl

--- tcl4data/orientation.tcl
+++ tcl4data/orientation.tcl
@@ -1,33 +1,43 @@
-proc ::OrientationWatch::draw_orientation {mol sel {color red} {scale 1.0} {radius 0.2} {orientationidx 0}} {
-    variable orientationvalue
-    variable orientationcenter
+mol delete all
+mol new all_f_07.lammpstrj first 0 last 10 waitfor all
+
+#proc draw_orientation {args} {
+    #global vmd_frame
 
     set res 6
     set gidlist {}
     set filled yes
+    graphics 0 delete all
 
-    # perhaps this should use the center information
-    if {[catch {measure center $sel weight mass} center]} {
-        if {[catch {measure center $sel} center]} {
-            puts stderr "problem computing orientation center: $center"
-            return {}
-        }
-    }
-    set orientationvalue($orientationidx) [format "%6.2f D" [veclength $vector]]
-    set vechalf [vecscale [expr $scale * 0.5] $vector]
-    set now [molinfo $mol get frame]
-    set next [expr $now+1]
+    set now $vmd_frame(0); set next [expr $now+1]
 
-    lappend gidlist [graphics $mol color $color]
-    set atomlist [[atomselect $mol $sel] get index]
+    lappend gidlist [graphics 0 color red]
+    set atomlist [[atomselect 0 "type 1"] get index]
     foreach loop $atomlist {
-        set start [lindex [[atomselect $mol "index $loop" frame $now]  get {x y z}] 0]
-		set end   [lindex [[atomselect $mol "index $loop" frame $next] get {x y z}] 0]
+        
+        ############ we need deal with the PBC coordinate#############
+        set now_x  [[atomselect 0 "index $loop" frame 1]  get x]
+        set now_y  [[atomselect 0 "index $loop" frame 1]  get y] 
+        set now_z  [[atomselect 0 "index $loop" frame 1]  get z] 
+        set next_x [[atomselect 0 "index $loop" frame 2]  get x]
+        set next_y [[atomselect 0 "index $loop" frame 2]  get y] 
+        set next_z [[atomselect 0 "index $loop" frame 2]  get z]
+        if {}
+        
+        set start {}
+		set end   {}
+        lappend start $now_x
+        lappend start $now_y
+        lappend start $now_z
+        lappend end   $next_x
+        lappend end   $next_y
+        lappend end   $next_z
 		set middle [vecadd $start [vecscale 0.9 [vecsub $end $start]]]
-		lappend gidlist [graphics $mol cylinder $start $middle radius 0.05]
-		lappend gidlist [graphics $mol cone $middle $end radius 0.08]
+		graphics 0 cylinder $start $middle radius 0.05
+		graphics 0 cone $middle $end radius 0.08
+        puts "fuck!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         
     }
+#}
 
-    return $gidlist
-}
+#trace variable vmd_frame(0) w draw_orientation
